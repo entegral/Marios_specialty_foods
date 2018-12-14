7e8f6a15989e0195784448ff11ce9bbb62368a8b
Rob <e2robby@gmail.com>
2018-12-14 11:10:55 -0800

    add faker seeding and additional review validation rule for content_body; must db:reset before seeding with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.dish, cost: Faker::Number.number(1), origin: Faker::Address.country)
+end
+
+250.times do |index|
+  Review.create!(author: Faker::Name.name, content_body: Faker::Lorem.characters(150), rating: 1+rand(5), product_id: 1+rand(50))
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
